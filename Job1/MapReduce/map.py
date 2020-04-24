845d669d35b682139d9c42087e4ee92fdc933169
federico <federicocialini@gmail.com>
2020-04-24 21:26:31 +0200

    first job commit mapper

---
+++ Job1/MapReduce/map.py
@@ -0,0 +1,8 @@
+import sys
+
+lines = sys.stdin.readlines()
+prices = lines[1:]
+for line in prices:
+    Ticker, OpenValue, CloseValue, Adj_close, LowThe, HighThe, Volume, Date = line.split(",")
+    DailyVariance = float(CloseValue) - float(OpenValue)
+    print(Ticker + str(DailyVariance) + LowThe + HighThe + Volume)
