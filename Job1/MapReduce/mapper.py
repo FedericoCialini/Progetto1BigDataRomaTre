1cecc95081dafe3bb706ad2a4029d964d373b995
federico <federicocialini@gmail.com>
2020-04-25 18:23:51 +0200

    remove date

--- Job1/MapReduce/mapper.py
+++ Job1/MapReduce/mapper.py
@@ -7,5 +7,5 @@ for line in prices:
     Ticker, OpenValue, CloseValue, Adj_close, LowThe, HighThe, Volume, Date = line.strip().split(",")
     # date = datetime.datetime.strptime(Date,'%Y-%m-%d') //troppo lenta!
     year = Date.split("-")[0]
-    if int(year) >= 2008:
-        print(Ticker, OpenValue, CloseValue, LowThe, HighThe, Volume, Date, sep='\t')
+    if year >= '2008':
+        print(Ticker, OpenValue, CloseValue, LowThe, HighThe, Volume, sep='\t')
--- Job1/MapReduce/reducer.py
+++ Job1/MapReduce/reducer.py
@@ -2,14 +2,13 @@ import sys
 
 
 class DailyPrice:
-    def __init__(self, ticker, openvalue, closevalue, lowthe, highthe, volume, date):
+    def __init__(self, ticker, openvalue, closevalue, lowthe, highthe, volume):
         self.ticker = ticker
         self.openvalue = openvalue
         self.closevalue = closevalue
         self.lowthe = lowthe
         self.highthe = highthe
         self.volume = volume
-        self.date = date
 
     def __eq__(self, other):
         return self.ticker == other.ticker and self.openvalue == other.openvalue and self.closevalue == other.closevalue and self.lowthe == other.lowthe and self.highthe == other.highthe
@@ -20,26 +19,24 @@ class DailyPrice:
 
 h = {}
 for line in sys.stdin.readlines():
-    Ticker, OpenValue, CloseValue, LowThe, HighThe, volume, Date = line.strip().split("\t")
-    price = DailyPrice(Ticker, OpenValue, CloseValue, LowThe, HighThe, volume, Date)
+    Ticker, OpenValue, CloseValue, LowThe, HighThe, volume = line.strip().split("\t")
+    price = DailyPrice(Ticker, OpenValue, CloseValue, LowThe, HighThe, volume)
     h[Ticker] = h.get(Ticker, []) + [price]
 finalList = []
 for Ticker in h.keys():
     prices = h[Ticker]
-    dates = []
     maxvalues = []
     minvalues = []
     volumes = []
     for price in prices:
-        dates.append(price.date)
         volumes.append(float(price.volume))
         maxvalues.append(float(price.highthe))
         minvalues.append(float(price.lowthe))
     openValue = float(prices[0].closevalue)
     closeValue = float(prices[-1].closevalue)
     FinalVariance = round(((closeValue - openValue) / openValue) * 100)
-    finalList.append((Ticker + "\t" + "\t" + str(FinalVariance) + "%" + "\t" + str(max(maxvalues)) +"\t" + str(min(minvalues)) + "\t" +str(sum(volumes)/len(volumes)),FinalVariance))
-sortedList = sorted(finalList,key = lambda x: x[1],reverse=True)
+    finalList.append((Ticker + "\t" + "\t" + str(FinalVariance) + "%" + "\t" + str(min(minvalues)) + "\t" + str(
+        max(maxvalues)) + "\t" + str(sum(volumes) / len(volumes)), FinalVariance))
+sortedList = sorted(finalList, key=lambda x: x[1], reverse=True)
 for s in sortedList:
     print(s[0])
-
