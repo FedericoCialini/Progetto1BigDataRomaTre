74bed8a07492da0666802d10e6c02e08cb61c89a
federico <federicocialini@gmail.com>
2020-04-26 20:17:58 +0200

    refactor spark job1

--- Job1/Spark/job1Spark.py
+++ Job1/Spark/job1Spark.py
@@ -2,28 +2,40 @@ import pyspark
 import sys
 
 
-def finalVariance(InitiaValue, FinalValue):
-    InitiaValue = float(InitiaValue)
+def finalVariance(OpenValue, FinalValue):
+    OpenValue = float(OpenValue)
     FinalValue = float(FinalValue)
-    return round(((FinalValue - InitiaValue) / InitiaValue) * 100)
+    return round(((FinalValue - OpenValue) / OpenValue) * 100)
 
 
-def takecolumn(x, col):
-    List = []
-    for s in x:
-        List.append(s[col])
-    return List
+def TakeColumn(Lists, col):
+    ColumnList = []
+    for List in Lists:
+        ColumnList.append(List[col])
+    return ColumnList
 
 
-spark = pyspark.sql.SparkSession.builder.appName("Job1Spark").getOrCreate()
-lines = spark.read.csv(sys.argv[1], header=True).rdd.cache()
-tickers = lines.filter(lambda x: x[7].split("-")[0] >= '2008' and len(x) == 8).map(
-    lambda x: (x[0], [[float(x[2]), float(x[4]), float(x[5]), float(x[6])]])).reduceByKey(
-    lambda x, y: list(x) + list(y)). \
-    map(lambda x: (
-    x[0], (finalVariance(x[1][0][0], x[1][-1][0]), min(takecolumn(x[1], 1)), max(takecolumn(x[1], 2)),
-           (sum(takecolumn(x[1], 3)) / len(x[1]))))).collect()
-tickers = sorted(tickers,key=lambda x : x[1][0],reverse=True )
-for i in range(0, len(tickers)):
-    print(tickers[i][0], str(tickers[i][1][0]) + "%", tickers[i][1][1], tickers[i][1][2], tickers[i][1][3], sep='\t')
-spark.stop()
+def load_data(Spark):
+    lines = Spark.read.csv(sys.argv[1], header=True).rdd.cache()
+    return lines
+
+
+def elaborate(Spark):
+    Tickers = load_data(Spark) \
+        .filter(lambda x: x[7].split("-")[0] >= '2008') \
+        .map(lambda x: (x[0], [[float(x[2]), float(x[4]), float(x[5]), float(x[6])]])) \
+        .reduceByKey(lambda x, y: x + y) \
+        .map(lambda x: (x[0], (finalVariance(x[1][0][0], x[1][-1][0]), min(TakeColumn(x[1], 1)),
+                               max(TakeColumn(x[1], 2)), (sum(TakeColumn(x[1], 3)) / len(x[1]))))) \
+        .collect()
+    Tickers = sorted(Tickers, key=lambda x: x[1][0], reverse=True)
+    return Tickers
+
+
+if __name__ == "__main__":
+    spark = pyspark.sql.SparkSession.builder.appName("Job1Spark").getOrCreate()
+    tickers = elaborate(spark)
+    for i in range(0, len(tickers)):
+        print(tickers[i][0], str(tickers[i][1][0]) + "%", tickers[i][1][1], tickers[i][1][2], tickers[i][1][3],
+              sep='\t')
+    spark.stop()
