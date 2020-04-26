cb283ca6781edab2cd196b8ef8f8ee3a235df783
federico <federicocialini@gmail.com>
2020-04-26 16:23:32 +0200

    spark job1

--- Job1/Spark/job1Spark.py
+++ Job1/Spark/job1Spark.py
@@ -2,9 +2,24 @@ import pyspark
 import sys
 from operator import add
 
-args = str(sys.argv)
-spark = pyspark.sql.SparkSession.builder.appName("Job1Spark").getOrCreate()
-lines = spark.read.text(sys.argv[1]).rdd.map(lambda row : row[0]).take(100)
 
-print(lines)
+def finalVariance(InitiaValue, FinalValue):
+    InitiaValue = float(InitiaValue)
+    FinalValue = float(FinalValue)
+    print(InitiaValue)
+    print((FinalValue))
+    return str(round(((FinalValue - InitiaValue) / InitiaValue) * 100)) + "%"
+
+
+spark = pyspark.sql.SparkSession.builder.appName("Job1Spark").getOrCreate()
+lines = spark.read.csv(sys.argv[1], header=True).rdd.cache()
+listoflists = []
+tickers = lines.filter(lambda x: x[7].split("-")[0] >= '2008' and len(x) == 8).map(
+    lambda x: (x[0], [[float(x[2]), float(x[4]), float(x[5]), float(x[6])]])).reduceByKey(
+    lambda x, y: list(x) + list(y)). \
+    map(lambda x: (
+x[0], (finalVariance(x[1][0][0], x[1][-1][0]), min(x[1][1]), max(x[1][2]), (sum(x[1][3]) / len(x[1]))))).collect()
+print(tickers)
+
 print("----------------------------------------------")
+sc = spark.sparkContext
