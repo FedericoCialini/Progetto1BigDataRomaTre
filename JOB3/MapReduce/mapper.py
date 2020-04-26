037f8c9524b7a50c027c88964f7f3d732ea01247
federico <federicocialini@gmail.com>
2020-04-26 21:22:45 +0200

    job3 mapper

---
+++ JOB3/MapReduce/mapper.py
@@ -0,0 +1,16 @@
+#!/home/federico/anaconda3/bin/python
+import sys
+
+
+def mapping():
+    lines = sys.stdin.readlines()
+    prices = lines[1:]
+    for line in prices:
+        Ticker, OpenValue, CloseValue, Adj_close, LowThe, HighThe, Volume, Date = line.strip().split(",")
+        year = Date.split("-")[0]
+        if year >= '2016':
+            print(Ticker, OpenValue, CloseValue, Date, sep='\t')
+
+
+if __name__ == '__main__':
+    mapping()
